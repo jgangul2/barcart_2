b68d03d1240506525be2cfdcf5747f1f2ba167a3
anonymous anonymous <jgangul2@illinois.edu>
2022-03-14 22:54:29 +0000

    Whitelist columns in CocktailFavoritesUser for ActiveAdmin

--- app/admin/cocktail_favorites_users.rb
+++ app/admin/cocktail_favorites_users.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register CocktailFavoritesUser do
 
+ permit_params :user_id, :cocktail_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
