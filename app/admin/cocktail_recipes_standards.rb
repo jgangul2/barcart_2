3bb27502541a62dc8a8b3d278152e25b3d3b5d81
anonymous anonymous <jgangul2@illinois.edu>
2022-03-14 22:54:27 +0000

    Whitelist columns in CocktailRecipesStandard for ActiveAdmin

--- app/admin/cocktail_recipes_standards.rb
+++ app/admin/cocktail_recipes_standards.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register CocktailRecipesStandard do
 
+ permit_params :cocktail_id, :ingredient_id, :quantity
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
