1ba4efcd362ddbbdd8032d6f060d22035024c7ef
anonymous anonymous <jgangul2@illinois.edu>
2022-03-14 22:54:58 +0000

    Add foreign key cocktail_id reference to user_customized_cocktails table

---
+++ db/migrate/20220314225458_add_cocktail_reference_to_user_customized_cocktails.rb
@@ -0,0 +1,7 @@
+class AddCocktailReferenceToUserCustomizedCocktails < ActiveRecord::Migration[6.0]
+  def change
+    add_foreign_key :user_customized_cocktails, :cocktail_details, column: :cocktail_id
+    add_index :user_customized_cocktails, :cocktail_id
+    change_column_null :user_customized_cocktails, :cocktail_id, false
+  end
+end
