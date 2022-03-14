f24f5adc1adeff5deb809d4fc902b45f527613fb
anonymous anonymous <jgangul2@illinois.edu>
2022-03-14 22:54:23 +0000

    Whitelist columns in CocktailDetail for ActiveAdmin

--- app/admin/cocktail_details.rb
+++ app/admin/cocktail_details.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register CocktailDetail do
 
+ permit_params :name, :description, :image_url
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
