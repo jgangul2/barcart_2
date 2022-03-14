f7030960b0a441a319bf8fb00b99838920a42945
anonymous anonymous <jgangul2@illinois.edu>
2022-03-14 22:54:34 +0000

    Whitelist columns in SpiritsBrand for ActiveAdmin

--- app/admin/spirits_brands.rb
+++ app/admin/spirits_brands.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register SpiritsBrand do
 
+ permit_params :spirit_id, :name, :description
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
