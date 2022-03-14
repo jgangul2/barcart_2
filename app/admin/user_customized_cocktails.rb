604e1a21297e6605bb81e5fb9f2f31616609e142
anonymous anonymous <jgangul2@illinois.edu>
2022-03-14 22:54:38 +0000

    Whitelist columns in UserCustomizedCocktail for ActiveAdmin

--- app/admin/user_customized_cocktails.rb
+++ app/admin/user_customized_cocktails.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register UserCustomizedCocktail do
 
+ permit_params :user_id, :cocktail_id, :ingredient_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
