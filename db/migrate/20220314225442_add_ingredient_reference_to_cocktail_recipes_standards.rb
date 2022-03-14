cbe05e767d6366b4f9296a40c5bbef3d7c65fd38
anonymous anonymous <jgangul2@illinois.edu>
2022-03-14 22:54:42 +0000

    Add foreign key ingredient_id reference to cocktail_recipes_standards table

---
+++ db/migrate/20220314225442_add_ingredient_reference_to_cocktail_recipes_standards.rb
@@ -0,0 +1,7 @@
+class AddIngredientReferenceToCocktailRecipesStandards < ActiveRecord::Migration[6.0]
+  def change
+    add_foreign_key :cocktail_recipes_standards, :spirits_details, column: :ingredient_id
+    add_index :cocktail_recipes_standards, :ingredient_id
+    change_column_null :cocktail_recipes_standards, :ingredient_id, false
+  end
+end
